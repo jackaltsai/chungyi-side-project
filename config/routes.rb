4c0812f25722b37e483c117b2708c577e407feea
jackaltsai <hata.s520@gmail.com>
2021-03-15 00:26:07 +0800

    add column image to project table

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "projects#index"
 
   namespace :admin do
-    resources: projects
+    resources :projects
     root "projects#index"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_151411) do
+ActiveRecord::Schema.define(version: 2021_03_14_162303) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
