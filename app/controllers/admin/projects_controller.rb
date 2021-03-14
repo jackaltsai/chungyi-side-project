288f278f201ba121fdf85fe45b6afd8bd34081af
jackaltsai <hata.s520@gmail.com>
2021-03-14 23:50:45 +0800

    add project new and create actions and views fot admin

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -4,4 +4,25 @@ class Admin::ProjectsController < ApplicationController
     def index
         @projects = Project.all
     end
+
+    def new
+        @Project = Project.new
+    end
+
+    def create
+        @project = Project.new(project_params)
+        if @project.save
+          flash[:notice] = "project was successfully created"
+          redirect_to admin_projects_path
+        else
+          flash.now[:alert] = "project was failed to create"
+          render :new
+        end
+      end
+    
+      private
+    
+      def project_params
+        params.require(:project).permit(:name, :description)
+      end
 end
