76804f50cab0f42f4eaf84ed8398252e48143631
jackaltsai <hata.s520@gmail.com>
2021-03-15 12:04:46 +0800

    add footer and index card style

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
     before_action :authenticate_user!
     def index
-        
+        @projects = Project.page(params[:page]).per(9)
     end
 end
